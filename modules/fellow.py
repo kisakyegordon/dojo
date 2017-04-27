909b353596bd42448b650a2c9d7d101252b8fde9
kisakyegordon <gordon56692@gmail.com>
2017-04-27 15:57:16 +0300

    application modules

--- modules/fellow.py
+++ modules/fellow.py
@@ -2,7 +2,21 @@ from modules.person import Person
 
 
 class Fellow(Person):
-    def __init__(self, name):
+    def __init__(self, name, wants_living, gender=None ):
 
+        yes = 'Y'
+        no = 'N'
+        male = 'M'
+        female = 'F'
+
+        if(wants_living == yes or wants_living == no):
+            self.wants_living == wants_living
+        else:
+            raise ValueError("Entered value is incorrect, please use lower case")
+
+        if(gender == male or gender == female):
+            self.gender = gender
+        else:
+            raise ValueError("Entered gender value is incorrect")
 
         super(Fellow, self).__init__(name)
--- modules/person.py
+++ modules/person.py
@@ -1,6 +1,8 @@
 class Person(object):
 
     def __init__(self, name):
+
+#        self.gender = gender
         if isinstance(name, str):
             self.name = name
         else:
