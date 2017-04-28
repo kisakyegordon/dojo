7f9a73d7de303cef335b698d55c8d1f81ae7599e
kisakyegordon <gordon56692@gmail.com>
2017-04-28 05:01:24 +0300

    includes all the main functions

--- models/dojo.py
+++ models/dojo.py
@@ -1,55 +1,40 @@
+from .person import *
+from .room import *
 import random
-from room import LivingSpace, Office
-from person import Fellow, Staff
 
 
 class Dojo(object):
-    def __init__(self):
-        self.all_rooms = []
-        self.empty_rooms = []
-        self.offices = []
-        self.empty_offices = []
-        self.livingspaces = []
-        self.empty_livingspaces = []
-        self.initial_room_count = 0
-        self.staffs = []
-        self.fellows = []
-        self.all_people = []
-        self.unallocated_person = []
+    all_rooms = {}
+    staff = []
+    fellows = []
+    staff_and_fellows = {}
+    unallocated_people = {}
+    office_rooms = defaultdict(list)
+    living_space_rooms = defaultdict(list)
 
-    def create_room(self, room_type, *args):
-        """Check that the room does not exist and determine what type of room it is"""
-        print(args)
-        for rname in args:
+    @staticmethod
+    def create_room(room_type, room_name):
+        '''
+        Check that the room does not exist and determine what type of room it is
 
-            if rname not in [room.room_name for room in self.all_rooms]:
-                # Create Room
-                print ("Creating room")
-                new_room = Office(room_type,rname) if room_type == "office" else LivingSpace(room_type,rname)
-                self.all_rooms.append(new_room)
+        '''
 
-            else:
-                print ("Room with name {0} already exists".format(rname))
-        # new_rooms = []
-        # for room in args["<room_name>"]:
-        #     if room.lower() in [k.room_name.lower() for k in self.all_rooms]:
-        #         print ("Already exists!")
-        #         return
-        #
-        # if args["<room_type>"] == "Office":
-        #     new_room = Office(room)
-        #     self.offices.append(new_room)
-        #
-        # elif args["<room_type>"] == Living:
-        #     new_room = Living(room)
-        #     self.livingspaces.append(new_room)
-        #
-        # self.all_rooms.append(new_room)
-        # new_rooms.append(new_room)
+        for room_name in room_name:
+            # room_name=name_of_room[0]
+            if room_name in Dojo.all_rooms:
+                print('Room already exists')
+                return 'Room already exists'
+            elif room_type == 'office':
 
+                current_room = Office(room_name)
+                Dojo.all_rooms[current_room.room_name.upper()] = current_room.room_type
+                Dojo.office_rooms[current_room.room_name.upper()]
 
+                print('Office called %s created succesfully' % room_name)
 
+            elif room_type == 'livingspace':
+                current_room = LivingSpace(room_name)
+                Dojo.all_rooms[current_room.room_name.upper()] = current_room.room_type
+                Dojo.living_space_rooms[current_room.room_name.upper()]
+                print('Living space called %s created succesfully' % room_name)
 
-class Calculator(object):
-    def add(self, x, y):
-        return x + y
