034788fafc465385ce6f267be022babe143d5155
kisakyegordon <gordon56692@gmail.com>
2017-04-28 05:35:27 +0300

    mofified tests

--- tests/all_tests.py
+++ tests/all_tests.py
@@ -1,44 +1,17 @@
 import unittest
-import os
-import sys
-import inspect
-currentdir = os.path.dirname(os.path.abspath(
-   inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0, parentdir)
+from unittest import TestCase
 from models.dojo import Dojo
+import unittest.mock as mock
+import os
+from collections import defaultdict
 
 
+class TestDojo(TestCase):
+    def test_create_office(self):
+        previous_room_count = len(Dojo.all_rooms)
+        self.assertFalse('blue_office' in Dojo.all_rooms)
+        Dojo.create_room('office', 'blue_office')
+        self.assertTrue('blue_office'.upper() in Dojo.all_rooms)
+        new_room_count = len(Dojo.all_rooms)
+        self.assertEqual(previous_room_count + 1, new_room_count)
 
-class TestDojo(unittest.TestCase):
-
-    def setUp(self):
-        self.test_dojo = Dojo()
-
-    # Test Room Creation
-    def test_create_room(self):
-        initial_room_count = len(self.test_dojo.all_rooms)
-        blue_office = self.test_dojo.create_room("office", "blue")
-        #self.assertTrue(blue_office, msg="not true")
-        new_room_count = len(self.test_dojo.all_rooms)
-        self.assertEqual(new_room_count - initial_room_count, 1)
-
-    def test_create_living_space(self):
-        self.test_dojo.create_room({
-        "<room_type>": ["LivingSpace"],
-        "Living": True,
-        "Office": False
-    })
-
-    def test_create_office_space(self):
-        self.test_dojo.create_room({
-        "<room_type>": ["Office"],
-        "Living": False,
-        "Office": True
-    })
-
-
-
-
-if __name__ == '__main__':
-    unittest.main()
