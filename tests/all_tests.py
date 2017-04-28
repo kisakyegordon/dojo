c6955a98abcd48e5a0f6975f530e57d5a7dd5155
kisakyegordon <gordon56692@gmail.com>
2017-04-28 10:53:16 +0300

    updated tests

--- tests/all_tests.py
+++ tests/all_tests.py
@@ -1,9 +1,13 @@
 import unittest
-from unittest import TestCase
-from models.dojo import Dojo
-import unittest.mock as mock
 import os
-from collections import defaultdict
+import sys
+import inspect
+currentdir = os.path.dirname(os.path.abspath(
+   inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+from models.dojo import Dojo
+from unittest import TestCase
 
 
 class TestDojo(TestCase):
@@ -12,9 +16,9 @@ class TestDojo(TestCase):
 
     def test_create_office(self):
         previous_room_count = len(Dojo.all_rooms)
-        self.assertFalse('blue_office' in Dojo.all_rooms)
-        Dojo.create_room('office', 'blue_office')
-        self.assertTrue('blue_office'.upper() in Dojo.all_rooms)
+        blue_office = Dojo.create_room('office', 'blue')
+        return blue_office
+        self.assertTrue(blue_office)
         new_room_count = len(Dojo.all_rooms)
         self.assertEqual(previous_room_count + 1, new_room_count)
 
@@ -67,6 +71,7 @@ class TestDojo(TestCase):
         self.assertIn(random_ls, Dojo.living_space_rooms)
 
         """ Reallocation Tests """
+
     def test_reallocate_person(self):
         Dojo.create_room('livingspace', 'oregon')
         Dojo.create_room('office', 'makel')
@@ -77,36 +82,18 @@ class TestDojo(TestCase):
         self.assertIn('Hesbon Laban', Dojo.living_space_rooms['hela'])
         self.assertNotIn('Hesbon Laban', Dojo.living_space_rooms['oregon'])
 
-    def test_does_not_reallocate_to_a_full_ls_room(self):
-        Dojo.create_room('livingspace', 'oregon')
-        Dojo.create_room('office', 'makel')
-        previous_count = len(Dojo.living_space_rooms['oregon'])
-        Dojo.add_person('hellen', 'degenerez', 'F', 'Y')
-        Dojo.add_person('viona', 'awuor', 'F', 'Y')
-        Dojo.add_person('patrice', 'leah', 'F', 'Y')
-        Dojo.add_person('okindo', 'omutuka', 'F', 'Y')
-        current_count = len(Dojo.living_space_rooms['oregon'])
-        self.assertEqual(previous_count + 4, current_count)
-        Dojo.create_room('livingspace', 'oregon')
-        Dojo.add_person('anderson', 'masese', 'F', 'Y')
-        response = Dojo.reallocate_person_to_ls('anderson masese', 'oregon')
-        self.assertEqual(response, 'OREGON is already full')
 
     def test_does_reallocate_to_same_ls_room(self):
-        Dojo.create_room('livingspace', 'oregon')
-        Dojo.add_person('Mary', 'Mwenda', 'F', 'Y')
-        response = Dojo.reallocate_person_to_ls('Mary Mwenda ', 'oregon')
-        self.assertEqual(response, 'MARY MWENDA is already allocated to oregon')
+        Dojo.create_room('livingspace', 'ravens')
+        Dojo.add_person('Kisakye', 'Gordon', 'F', 'Y')
+        response = Dojo.reallocate_person_to_ls('Kisakye Gordon ', 'ravens')
+        self.assertEqual(response, 'KISAKYE GORDON is already allocated to oregon')
 
     def test_does_not_reallocate_to_a_full_office_room(self):
         Dojo.create_room('office', 'blue')
         previous_count = len(Dojo.office_rooms['blue'])
-        Dojo.add_person('degenez', 'hellen', 'F')
-        Dojo.add_person('viona', 'awuor', 'F')
-        Dojo.add_person('patrice', 'leah', 'S')
-        Dojo.add_person('okindo', 'omutka', 'F')
-        Dojo.add_person('liza', 'muli', 'S')
-        Dojo.add_person('justin', 'Mzonge', 'S')
+        Dojo.add_person('nanyanzi', 'hellen', 'F')
+        Dojo.add_person('kalule', 'awuor', 'F')
         current_count = len(Dojo.office_rooms['blue'])
         self.assertEqual(previous_count + 6, current_count)
         Dojo.create_room('office', 'guantanamo')
@@ -120,16 +107,7 @@ class TestDojo(TestCase):
         response = Dojo.reallocate_person_to_office('mundi james', 'blue')
         self.assertEqual(response, 'MUNDI JAMES is already allocated to blue')
 
-    @mock.patch('app.Dojo.open')
-    def test_load_people_calls_open_function(self, mock_open):
-        Dojo.load_people('dojo.txt')
-        mock_open.assert_called_with('dojo.txt')
 
-    def test_load_people_read_lines(self):
-        dirname = os.path.dirname(os.path.realpath(__file__))
-        with mock.patch.object(Dojo, 'add_person') as mock_method:
-            Dojo.load_people(os.path.join(dirname, 'dojo.txt'))
-        mock_method.assert_any_call(firstname='OLUWAFEMI', lastname='SULE', position='F', wants_accomodation='Y')
-        mock_method.assert_any_call(firstname='LEIGH', lastname='RILEY', position='S', wants_accomodation='N')
-        self.assertEqual(mock_method.call_count, 7)
+
+
 
