33aab9f2d8040a880682b9e8640391ccd28a7427
kisakyegordon <gordon56692@gmail.com>
2017-04-28 05:50:13 +0300

    main application

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
 """
-This example uses docopt with the built in cmd module to demonstrate an
-interactive command application.
 Usage:
-    the_dojo create_room <room_type> <room_name>...
-    the_dojo add_person <person_name> (FELLOW|STAFF) [wants_accommodation]
-    the_dojo (-i | --interactive)
-    the_dojo (-h | --help | --version)
-Options:
-    -i, --interactive  Interactive Mode
-    -h, --help  Show this screen and exit.
+    create_room <room_type> <room_name>
+    add_person <person_id> <first_name> <last_name> <F|S> [--wants_accomodation=N]
+    reallocate_person <firstname> <lastname> <new_room_name>
+    load_people <filename>
+    print_allocations [--o=filename]
+    print_unallocated [--o=filename]
+    print_room <room_name>
+    quit
 """
 
-import sys
-import cmd
-from termcolor import cprint
-from pyfiglet import figlet_format
 from models.dojo import Dojo
+import sys, cmd, os
+from termcolor import cprint, colored
+from pyfiglet import figlet_format
 from docopt import docopt, DocoptExit
-
-my_dojo_object = Dojo()
 
 
 def docopt_cmd(func):
@@ -30,12 +26,12 @@ def docopt_cmd(func):
         try:
             opt = docopt(fn.__doc__, arg)
 
-        except DocoptExit as exit:
+        except DocoptExit as e:
             # The DocoptExit is thrown when the args do not match.
             # We print a message to the user and the usage block.
 
-            print('You have entered an invalid command!')
-            print(exit)
+            print('Invalid Command!')
+            print(e)
             return
 
         except SystemExit:
@@ -51,38 +47,29 @@ def docopt_cmd(func):
     fn.__dict__.update(func.__dict__)
     return fn
 
+def introduction():
+     print (__doc__)
 
-class Thedojo (cmd.Cmd):
+def save_state_on_interrupt():
+    print("saving state...")
+    Dojo.save_state()
 
+
+class DojoApplication(cmd.Cmd):
     cprint(figlet_format('the\n' + 'Dojo\n', font='univers'), 'green', attrs=['bold'])
-    intro = ('Welcome To The Dojo \n\nType "help" To View More Commands or "quit" To Exit Applicaton \n\n------------------'
-             '\n\nCOMMANDS \n\n------------------ \n\n1 - add_person <person_name> (FELLOW|STAFF) [wants_accommodation] \n2 - create_room <room_type> <room_name>...')
     prompt = '(the_dojo) '
 
 
 
-    file = None
-
-    @docopt_cmd
-    def do_add_person(self, args):
-        """Usage: add_person <person_name> (FELLOW|STAFF) [wants_accommodation]"""
-        print(args)
-
-    @docopt_cmd
-    def do_create_room(self, args):
-        """Usage: create_room <room_type> <room_name>..."""
-
-        for room_name in args["<room_name>"]:
-            my_dojo_object.create_room(["<room_type>"], room_name)
-
     def do_quit(self, arg):
-        """Quit from interface."""
+        '''Usage: quit application '''
 
         exit()
 
-opt = docopt(__doc__, sys.argv[1:])
 
-if opt['--interactive']:
-    Thedojo().cmdloop()
-
-print(opt)
+if __name__ == '__main__':
+    introduction()
+    try:
+        DojoApplication().cmdloop()
+    except KeyboardInterrupt:
+        save_state_on_interrupt()
