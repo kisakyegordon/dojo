22b6ab6462d7b30dd5ceb3ded0c6bd9dd8a009e8
kisakyegordon <gordon56692@gmail.com>
2017-04-28 05:55:57 +0300

    main application

--- main.py
+++ main.py
@@ -59,8 +59,75 @@ class DojoApplication(cmd.Cmd):
     cprint(figlet_format('the\n' + 'Dojo\n', font='univers'), 'green', attrs=['bold'])
     prompt = '(the_dojo) '
 
+    """ Calling the create_room function from models/dojo.py """
+    @docopt_cmd
+    def do_create_room(self, arg):
+        '''Usage: create_room <room_type> <room_name>...'''
+        room_name = arg["<room_name>"]
+        room_type = arg["<room_type>"]
+        Dojo.create_room(room_type, room_name)
 
+    """ Calling the add_person function from models/dojo.py """
+    @docopt_cmd
+    def do_add_person(self, arg):
+        '''Usage: add_person <firstname> <lastname> <position> [--wants_accomodation=N] '''
 
+        first_name = arg["<firstname>"]
+        last_name = arg["<lastname>"]
+        pos = arg["<position>"]
+        wants_accomodation = arg["--wants_accomodation"]
+        Dojo.add_person(first_name, last_name, pos.upper(), str(wants_accomodation))
+
+    """ Calling the load_people function from models/dojo.py """
+    @docopt_cmd
+    def do_load_people(self, arg):
+        ''' Usage: load_people <filename>'''
+        file_name = arg["<filename>"]
+        if os.path.exists(file_name):
+            Dojo.load_people(file_name)
+        else:
+            print("File not found")
+
+    """ Calling the reallocate_person function from models/dojo.py """
+    @docopt_cmd
+    def do_reallocate_person(self, arg):
+        ''' Usage: reallocate_person <firstname> <lastname> <new_room_name>'''
+        first_name = arg["<firstname>"]
+        last_name = arg["<lastname>"]
+        full_name = first_name + " " + last_name
+        new_room = arg["<new_room_name>"]
+
+        if new_room.upper() in Dojo.office_rooms:
+            Dojo.reallocate_person_to_office(full_name.upper(), new_room.upper())
+        elif new_room.upper() in Dojo.ls_rooms:
+            Dojo.reallocate_person_to_ls(full_name.upper(), new_room.upper())
+        else:
+            print('%s is not a room in Dojo' % new_room)
+
+    """ Calling the print_room function from models/dojo.py """
+    @docopt_cmd
+    def do_print_room(self, arg):
+        ''' Usage: print_room <room_name>'''
+        room_name = arg["<room_name>"]
+        Dojo.print_room(room_name)
+
+    """ Calling the print_allocations function from models/dojo.py """
+    @docopt_cmd
+    def do_print_allocations(self, arg):
+        '''Usage: print_allocations [--o=filename] '''
+        filename = arg["--o"]
+
+        Dojo.print_allocations(filename)
+
+    """ Calling the print_unallocated function from models/dojo.py """
+    @docopt_cmd
+    def do_print_unallocated(self, arg):
+        '''Usage: print_unallocated [--o=filename] '''
+        filename = arg["--o"]
+
+        Dojo.print_unallocated(filename)
+
+    """ Exit From docopt """
     def do_quit(self, arg):
         '''Usage: quit application '''
 
